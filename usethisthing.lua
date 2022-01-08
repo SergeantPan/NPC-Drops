1d36a8e11f7cea2495e8c776d3d41f7e15fa12ce
SergeantPan <lassijokinen00@gmail.com>
2022-01-08 19:34:36 +0200

    Update usethisthing.lua
    Updated to reflect newest form

--- usethisthing.lua
+++ usethisthing.lua
@@ -1,16 +1,19 @@
 CreateConVar( "ChanceOfAmmo", "100", 128, "Chance of ammo drop 100 = 100%, 10 = 10% etc." )
 CreateConVar( "NPCDropAmmo", "1", 128, "Enable NPCs dropping ammo 1 = Enabled, 0 = Disabled" )
 CreateConVar( "PlayerDropAmmo", "1", 128, "Enable Players dropping ammo 1 = Enabled, 0 = Disabled" )
+CreateConVar( "DropGrenadeAmmo", "1", 128, "Enable players/NPC's with weapons that use SMG1 Alt-Fire to drop SMG1 grenades 1 = Enabled, 0 = Disabled" )
+CreateConVar( "DropAR2AltAmmo", "1", 128, "Enable players with weapons that use AR2 Alt-Fire to drop Energy balls 1 = Enabled, 0 = Disabled" )
 
 hook.Add( "OnNPCKilled", "TheyDied", function( npc )
 
-local PistolAmmo = ents.Create( "item_ammo_pistol" ) // Replace "item_ammo_pistol" with your own entity, if you want.
-local RevolverAmmo = ents.Create( "item_ammo_357" ) // Each one can be replaced as you wish
-local SmgAmmo = ents.Create( "item_ammo_smg1" ) // If you want a new one just use one of the existing ones as template
+local PistolAmmo = ents.Create( "item_ammo_pistol" ) // replace "item_ammo_pistol" with your own entity, if you want, same for all the others
+local RevolverAmmo = ents.Create( "item_ammo_357" )
+local SmgAmmo = ents.Create( "item_ammo_smg1" )
 local ArAmmo = ents.Create( "item_ammo_ar2" )
 local ShotgunAmmo = ents.Create( "item_box_buckshot" )
 local RocketAmmo = ents.Create( "item_rpg_round" )
 local BowAmmo = ents.Create( "item_ammo_crossbow" )
+local Grenadez = ents.Create( "item_ammo_smg1_grenade" )
 local AmmoChance = math.Rand( 1, 100 ) // Create this stuff
 
 if GetConVar( "NPCDropAmmo" ):GetInt() == 0 then return end // Do we allow drops?
@@ -19,42 +22,48 @@ if npc:GetActiveWeapon():IsWeapon() == false then return end // Does the NPC eve
 
 if AmmoChance > GetConVar( "ChanceOfAmmo" ):GetInt() then return end // Do we get lucky and get ammo?
 
-if npc:GetActiveWeapon():GetClass() == "weapon_pistol" then // Replace the "weapon_pistol" with whatever weapon your npc has
-PistolAmmo:SetPos( npc:GetPos() )  // This makes it spawn at their feet
-PistolAmmo:Spawn() 
-PistolAmmo:SetCollisionGroup( 1 ) // Disable collision with the NPC so the ammo doesn't go flying.
+if npc:GetActiveWeapon():GetSecondaryAmmoType() == 9 and GetConVar( "DropGrenadeAmmo" ):GetInt() == 1 then
+Grenadez:SetPos( npc:GetPos() )
+Grenadez:Spawn()
+Grenadez:SetCollisionGroup( 2 )
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_357" then // Rince and repeat
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 3 then // Being the massive dumbass I am, I didn't do this until now Rather than using the weapon as the basis, we look for the ammo type
+PistolAmmo:SetPos( npc:GetPos() )  // This makes it spawn at their feet // Fucking kill me This makes it mcuh better and universal, rather than restricting it to weapon types
+PistolAmmo:Spawn() 
+PistolAmmo:SetCollisionGroup( 1 ) // Disable collision with the NPC so the ammo doesn't go flying
+end
+
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 5 then // Rince and repeat, again
 RevolverAmmo:SetPos( npc:GetPos() )
 RevolverAmmo:Spawn()
 RevolverAmmo:SetCollisionGroup( 1 )
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_smg1" then
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 4 then
 SmgAmmo:SetPos( npc:GetPos() )
 SmgAmmo:Spawn()
 SmgAmmo:SetCollisionGroup( 1 )
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_ar2" then
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 1 then
 ArAmmo:SetPos( npc:GetPos() )
 ArAmmo:Spawn()
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_shotgun" then
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 7 then
 ShotgunAmmo:SetPos( npc:GetPos() )
 ShotgunAmmo:Spawn()
 ShotgunAmmo:SetCollisionGroup( 1 )
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_rpg" then
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 8 then
 RocketAmmo:SetPos( npc:GetPos() )
 RocketAmmo:Spawn()
 RocketAmmo:SetCollisionGroup( 1 )
 end
 
-if npc:GetActiveWeapon():GetClass() == "weapon_crossbow" then
+if npc:GetActiveWeapon():GetPrimaryAmmoType() == 6 then
 BowAmmo:SetPos( npc:GetPos() )
 BowAmmo:Spawn()
 BowAmmo:SetCollisionGroup( 1 )
@@ -70,6 +79,8 @@ local ArAmmo = ents.Create( "item_ammo_ar2" )
 local ShotgunAmmo = ents.Create( "item_box_buckshot" )
 local RocketAmmo = ents.Create( "item_rpg_round" )
 local BowAmmo = ents.Create( "item_ammo_crossbow" )
+local Grenadez = ents.Create( "item_ammo_smg1_grenade" )
+local ArBallz = ents.Create( "item_ammo_ar2_altfire" )
 local AmmoChance = math.Rand( 1, 100 )
 
 if GetConVar( "PlayerDropAmmo" ):GetInt() == 0 then return end
@@ -78,42 +89,54 @@ if ply:GetActiveWeapon():IsWeapon() == false then return end
 
 if AmmoChance > GetConVar( "ChanceOfAmmo" ):GetInt() then return end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_pistol" then // You get the point
+if ply:GetActiveWeapon():GetSecondaryAmmoType() == 9 and GetConVar( "DropGrenadeAmmo" ):GetInt() == 1 then
+Grenadez:SetPos( npc:GetPos() )
+Grenadez:Spawn()
+Grenadez:SetCollisionGroup( 2 )
+end
+
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 2 and GetConVar( "DropAR2AltAmmo" ):GetInt() == 1 then
+ArBallz:SetPos( ply:GetPos() )
+ArBallz:Spawn()
+ArBallz:SetCollisionGroup( 1 )
+end
+
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 3 then // You get the point
 PistolAmmo:SetPos( ply:GetPos() )
 PistolAmmo:Spawn()
 PistolAmmo:SetCollisionGroup( 1 )
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_357" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 5 then
 RevolverAmmo:SetPos( ply:GetPos() )
 RevolverAmmo:Spawn()
 RevolverAmmo:SetCollisionGroup( 1 )
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_smg1" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 4 then
 SmgAmmo:SetPos( ply:GetPos() )
 SmgAmmo:Spawn()
 SmgAmmo:SetCollisionGroup( 1 )
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_ar2" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 1 then
 ArAmmo:SetPos( ply:GetPos() )
 ArAmmo:Spawn()
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_shotgun" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 7 then
 ShotgunAmmo:SetPos( ply:GetPos() )
 ShotgunAmmo:Spawn()
 ShotgunAmmo:SetCollisionGroup( 1 )
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_rpg" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 8 then
 RocketAmmo:SetPos( ply:GetPos() )
 RocketAmmo:Spawn()
 RocketAmmo:SetCollisionGroup( 1 )
 end
 
-if ply:GetActiveWeapon():GetClass() == "weapon_crossbow" then
+if ply:GetActiveWeapon():GetPrimaryAmmoType() == 6 then
 BowAmmo:SetPos( ply:GetPos() )
 BowAmmo:Spawn()
 BowAmmo:SetCollisionGroup( 1 )
